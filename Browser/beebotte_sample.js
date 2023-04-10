038ce9baebb68a4056cca49335a5deaabc850515
kaoru1985 <outsider07.k@gmail.com>
2023-04-10 17:00:13 +0900

    feat jsFix2

--- Browser/beebotte_sample.js
+++ Browser/beebotte_sample.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function sendData1(status) {
           //URL末尾で、チャンネルとトピックを指定
       const url1 = 'https://api.beebotte.com/v1/data/publish/vollmont/coneunit';
-      const channelToken1 = 'your_token1'; // ここに実際のチャンネルトークンを入れてください
+      const channelToken1 = 'your_token'; // ここに実際のチャンネルトークンを入れてください
       fetch(url1, {
         method: 'POST',
         headers: {
@@ -34,39 +34,39 @@ document.addEventListener('DOMContentLoaded', () => {
           console.error('Request error:', error);
         });
     }
-    const onButton2 = document.getElementById('onButton2');
-    const offButton2 = document.getElementById('offButton2');
+    // const onButton2 = document.getElementById('onButton2');
+    // const offButton2 = document.getElementById('offButton2');
   
-    onButton2.addEventListener('click', () => {
-      sendData2('true');
-    });
-    offButton2.addEventListener('click', () => {
-      sendData2('false');
-    });
+    // onButton2.addEventListener('click', () => {
+    //   sendData2('true');
+    // });
+    // offButton2.addEventListener('click', () => {
+    //   sendData2('false');
+    // });
   
-    function sendData2(status) {
-          //URL末尾で、チャンネルとトピックを指定
-      const url2 = 'https://api.beebotte.com/v1/data/publish/vollmont/led';
-      const channelToken2 = 'your_token2'; // ここに実際のチャンネルトークンを入れてください
-      fetch(url2, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Auth-Token': channelToken2,
-        },
-        body: JSON.stringify({
-          data: status.toString(),
-        }),
-      })
-        .then((response) => {
-          if (response.ok) {
-            console.log('Request succeeded:', JSON.stringify({ data: status }));
-          } else {
-            console.error('Request failed:', response.status);
-          }
-        })
-        .catch((error) => {
-          console.error('Request error:', error);
-        });
-    }
+    // function sendData2(status) {
+    //       //URL末尾で、チャンネルとトピックを指定
+    //   const url2 = 'https://api.beebotte.com/v1/data/publish/vollmont/led';
+    //   const channelToken2 = 'your_token'; // ここに実際のチャンネルトークンを入れてください
+    //   fetch(url2, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'X-Auth-Token': channelToken2,
+    //     },
+    //     body: JSON.stringify({
+    //       data: status.toString(),
+    //     }),
+    //   })
+    //     .then((response) => {
+    //       if (response.ok) {
+    //         console.log('Request succeeded:', JSON.stringify({ data: status }));
+    //       } else {
+    //         console.error('Request failed:', response.status);
+    //       }
+    //     })
+    //     .catch((error) => {
+    //       console.error('Request error:', error);
+    //     });
+    // }
   });
