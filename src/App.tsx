0607c807dc45c010ec31a6b7cd771d6dc0d575f3
Yuma Takai <tara20070827@gmail.com>
2023-04-11 19:16:03 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SwitchMenuConeUnit from './components/SwitchMenuConeUnit';
+import SwitchMenuLED from './components/SwitchMenuLED';
+
+function App() {
+  return (
+    <div className="App">
+      <SwitchMenuConeUnit/>
+      <SwitchMenuLED/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SwitchMenuConeUnit.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+function SwitchMenuConeUnit(){
+    return (
+        <>
+            <h1>
+            ConeUnit
+            </h1>
+            <Button>
+                On
+            </Button>
+            <Button>
+                Off
+            </Button>
+        </>
+    );
+}
+
+export default SwitchMenuConeUnit;
